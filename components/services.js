8d8aa8b3de9b7c9a46292e9c0cf7209ced3ece1e
cahaug <caleahaug@gmail.com>
2019-05-19 17:31:44 -0700

    recommendation buttons complete

--- components/services.js
+++ components/services.js
@@ -5,3 +5,41 @@ function openNav() {
 function closeNav() {
     document.getElementById("myNav").style.width = "0%";
 };
+
+document.getElementById('groceryArticle').style.display='none';
+document.getElementById('deliveryArticle').style.display='none';
+document.getElementById('valetArticle').style.display='none';
+document.getElementById('waiterArticle').style.display='none';
+
+function groceryButton() {
+    var x = document.getElementById("groceryArticle");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+}
+function deliveryButton() {
+    var x = document.getElementById("deliveryArticle");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+}
+function valetButton() {
+    var x = document.getElementById("valetArticle");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+}
+function waiterButton() {
+    var x = document.getElementById("waiterArticle");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+}
