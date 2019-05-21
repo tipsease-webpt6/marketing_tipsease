af97276ecf8d40cb693fa1b481c713f6ba991065
Tyler Turnipseed <tyler@techturnip.us>
2019-05-21 19:15:56 -0400

    Added timer and initialized in on DOMContentLoaded for carousel component

--- components/carousel/carousel.js
+++ components/carousel/carousel.js
@@ -4,34 +4,31 @@ class Carousel {
     this.slideArr = this.carousel.querySelectorAll('.slide');
     this.slideIndex = 0;
     this.controls = this.carousel.querySelectorAll('.ctrl');
-    this.controls.forEach(ctrl => ctrl.addEventListener('click', e => {
-      if (!e.target.parentNode.parentNode.hasAttribute('disabled')) {
-        // only fire off the cycleSlide() method if the button is not disabled
-        this.cycleSlide(e)
-      }
+    this.init = document.addEventListener('DOMContentLoaded', () => {
+      this.timer();
 
-      e.preventDefault();
-    }));
+      this.controls.forEach(ctrl => ctrl.addEventListener('click', e => {
+        if (!e.target.parentNode.parentNode.hasAttribute('disabled')) {
+          // only fire off the cycleSlide() method if the button is not disabled
+          this.cycleSlide(e)
+        }
+
+        e.preventDefault();
+      }));
+    })
   }
 
-  cycleSlide(e) {
-    // Set variables to clean up logic
-    let ctrl = e.target.parentNode.parentNode;
-    let ctrlNum = this.slideArr.length - 1;
-
-    if (ctrl.classList.contains('next')) {
-      // disable the controls to prevent clicking until after animation finishes
-      ctrl.setAttribute('disabled', '');
+  timer() {
+    window.slideInterval = setInterval(() => {
       // Handle incrementing of slideIndex
-      if (this.slideIndex < ctrlNum) {
+      if (this.slideIndex < (this.slideArr.length - 1)) {
         this.slideIndex++;
       } else {
         this.slideIndex = 0;
       }
 
-      // Handle Next animation
-      this.slideArr.forEach((slide, i) => {
-        if (slide.classList.contains('active') && ctrl.classList.contains('next')) {
+      this.slideArr.forEach((slide) => {
+        if (slide.classList.contains('active')) {
           TweenLite.fromTo(slide, 1, {
             opacity: 1,
             x: 0
@@ -49,44 +46,101 @@ class Carousel {
                 opacity: 1,
                 x: 0
               });
-              ctrl.removeAttribute('disabled');
             }
           });
         }
       });
-    } else if (ctrl.classList.contains('prev')) {
-      ctrl.setAttribute('disabled', '');
-      // Handle incrementing of slideIndex
-      if (this.slideIndex > 0) {
-        this.slideIndex--;
-      } else {
-        this.slideIndex = ctrlNum;
+    }, 5000);
+  }
+
+  slideRight(btn, num) {
+    // disable the controls to prevent clicking until after animation finishes
+    btn.setAttribute('disabled', '');
+    // Handle incrementing of slideIndex
+    if (this.slideIndex < num) {
+      this.slideIndex++;
+    } else {
+      this.slideIndex = 0;
+    }
+
+    // Handle Next animation
+    this.slideArr.forEach((slide) => {
+      if (slide.classList.contains('active') && btn.classList.contains('next')) {
+        TweenLite.fromTo(slide, 1, {
+          opacity: 1,
+          x: 0
+        }, {
+          opacity: 0,
+          x: "-500px",
+          onComplete: () => {
+            // When animation finishes remove active class from current slide and apply it to the next slide and animate
+            slide.classList.remove('active');
+            this.slideArr[this.slideIndex].classList.add('active');
+            TweenLite.fromTo(this.slideArr[this.slideIndex], 1, {
+              opacity: 0,
+              x: "500px"
+            }, {
+              opacity: 1,
+              x: 0
+            });
+            btn.removeAttribute('disabled');
+            this.timer();
+          }
+        });
       }
-      // Handle Prev Animation
-      this.slideArr.forEach((slide, i) => {
-        if (slide.classList.contains('active') && ctrl.classList.contains('prev')) {
-          TweenLite.fromTo(slide, 1, {
-            opacity: 1,
-            x: 0
-          }, {
-            opacity: 0,
-            x: "500px",
-            onComplete: () => {
-              // When animation finishes remove active class from current slide and apply it to the prev slide and animate
-              slide.classList.remove('active');
-              this.slideArr[this.slideIndex].classList.add('active');
-              TweenLite.fromTo(this.slideArr[this.slideIndex], 1, {
-                opacity: 0,
-                x: "-500px"
-              }, {
-                opacity: 1,
-                x: 0
-              });
-              ctrl.removeAttribute('disabled');
-            }
-          });
-        }
-      });
+    });
+  }
+
+  slideLeft(btn, num) {
+    btn.setAttribute('disabled', '');
+
+    // Handle incrementing of slideIndex
+    if (this.slideIndex > 0) {
+      this.slideIndex--;
+    } else {
+      this.slideIndex = num;
+    }
+
+    // Handle Prev Animation
+    this.slideArr.forEach((slide) => {
+      if (slide.classList.contains('active') && btn.classList.contains('prev')) {
+        TweenLite.fromTo(slide, 1, {
+          opacity: 1,
+          x: 0
+        }, {
+          opacity: 0,
+          x: "500px",
+          onComplete: () => {
+            // When animation finishes remove active class from current slide and apply it to the prev slide and animate
+            slide.classList.remove('active');
+            this.slideArr[this.slideIndex].classList.add('active');
+            TweenLite.fromTo(this.slideArr[this.slideIndex], 1, {
+              opacity: 0,
+              x: "-500px"
+            }, {
+              opacity: 1,
+              x: 0
+            });
+            btn.removeAttribute('disabled');
+            this.timer();
+          }
+        });
+      }
+    });
+  }
+
+  cycleSlide(e) {
+    let ctrl = e.target.parentNode.parentNode;
+    let ctrlNum = this.slideArr.length - 1;
+
+    clearInterval(window.slideInterval);
+
+    if (ctrl.classList.contains('next')) {
+      this.slideRight(ctrl, ctrlNum);
+    }
+
+    if (ctrl.classList.contains('prev')) {
+      this.slideLeft(ctrl, ctrlNum);
     }
   }
 };
