d7fbedc46fff681bb9b477c260be03e95279c660
Tyler Turnipseed <tyler@techturnip.us>
2019-05-20 20:30:00 -0400

    Added overflow:hidden to body styling in base.less to prevent horizontal scroll bars when carousel animations fire off, removed this.nextBtn and this.prevBtn references and combined into this.controls reference adding the event listeners using a .forEach on the this.controls nodeList, added GSAP animations into the carousel logic

--- components/carousel/carousel.js
+++ components/carousel/carousel.js
@@ -3,10 +3,15 @@ class Carousel {
     this.carousel = carousel;
     this.slideArr = this.carousel.querySelectorAll('.slide');
     this.slideIndex = 0;
-    this.nextBtn = this.carousel.querySelector('.next');
-    this.prevBtn = this.carousel.querySelector('.prev');
-    this.nextBtn.addEventListener('click', e => this.cycleSlide(e));
-    this.prevBtn.addEventListener('click', e => this.cycleSlide(e));
+    this.controls = this.carousel.querySelectorAll('.ctrl');
+    this.controls.forEach(ctrl => ctrl.addEventListener('click', e => {
+      if (!e.target.parentNode.parentNode.hasAttribute('disabled')) {
+        // only fire off the cycleSlide() method if the button is not disabled
+        this.cycleSlide(e)
+      }
+
+      e.preventDefault();
+    }));
   }
 
   cycleSlide(e) {
@@ -14,29 +19,77 @@ class Carousel {
     let ctrl = e.target.parentNode.parentNode;
     let ctrlNum = this.slideArr.length - 1;
 
-    // Remove Active Class
-    this.slideArr.forEach(slide => slide.classList.remove('active'));
-
     if (ctrl.classList.contains('next')) {
+      // disable the controls to prevent clicking until after animation finishes
+      ctrl.setAttribute('disabled', '');
+      // Handle incrementing of slideIndex
       if (this.slideIndex < ctrlNum) {
         this.slideIndex++;
       } else {
         this.slideIndex = 0;
       }
 
-      this.slideArr[this.slideIndex].classList.add('active');
+      // Handle Next animation
+      this.slideArr.forEach((slide, i) => {
+        if (slide.classList.contains('active') && ctrl.classList.contains('next')) {
+          TweenLite.fromTo(slide, 1, {
+            opacity: 1,
+            x: 0
+          }, {
+            opacity: 0,
+            x: "-500px",
+            onComplete: () => {
+              // When animation finishes remove active class from current slide and apply it to the next slide and animate
+              slide.classList.remove('active');
+              this.slideArr[this.slideIndex].classList.add('active');
+              TweenLite.fromTo(this.slideArr[this.slideIndex], 1, {
+                opacity: 0,
+                x: "500px"
+              }, {
+                opacity: 1,
+                x: 0
+              });
+              ctrl.removeAttribute('disabled');
+            }
+          });
+        }
+      });
     } else if (ctrl.classList.contains('prev')) {
+      ctrl.setAttribute('disabled', '');
+      // Handle incrementing of slideIndex
       if (this.slideIndex > 0) {
         this.slideIndex--;
       } else {
         this.slideIndex = ctrlNum;
       }
-      this.slideArr[this.slideIndex].classList.add('active');
+      // Handle Prev Animation
+      this.slideArr.forEach((slide, i) => {
+        if (slide.classList.contains('active') && ctrl.classList.contains('prev')) {
+          TweenLite.fromTo(slide, 1, {
+            opacity: 1,
+            x: 0
+          }, {
+            opacity: 0,
+            x: "500px",
+            onComplete: () => {
+              // When animation finishes remove active class from current slide and apply it to the prev slide and animate
+              slide.classList.remove('active');
+              this.slideArr[this.slideIndex].classList.add('active');
+              TweenLite.fromTo(this.slideArr[this.slideIndex], 1, {
+                opacity: 0,
+                x: "-500px"
+              }, {
+                opacity: 1,
+                x: 0
+              });
+              ctrl.removeAttribute('disabled');
+            }
+          });
+        }
+      });
     }
-
-    e.preventDefault();
   }
-}
+};
 
 const carousels = document
   .querySelectorAll('.carousel')
